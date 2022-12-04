06c523b9ecf5139009f1bb1a19ec6fac9a02fc6f
Oksana Melnyk <kseniadanyluk1994@gmail.com>
2022-12-04 22:23:30 +0200

    created subscribe section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -76,3 +76,11 @@ $('.latest-news__slider').slick({
   ]
 });
 
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
+
